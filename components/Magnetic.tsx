7d08549c3896f8ba9d8d44503622348b92bd7d6e
ghostNotFounded <arsheya.par@gmail.com>
2024-04-04 08:16:03 +0530

    deployment issues

--- components/Magnetic.tsx
+++ components/Magnetic.tsx
@@ -4,14 +4,15 @@ import { useRef, useState } from "react";
 import { motion } from "framer-motion";
 
 const Magnetic = ({ children }: { children: React.ReactNode }) => {
-  const ref = useRef(null);
+  const ref = useRef<HTMLDivElement>(null);
   const [position, setPosition] = useState({ x: 0, y: 0 });
 
   const mouseMove = (e: any) => {
     if (ref.current) {
       const { clientX, clientY } = e;
-      // @ts-ignore
-      const { width, height, left, top } = ref.current.getBoundingClientRect();
+
+      const { width, height, left, top } =
+        ref?.current?.getBoundingClientRect();
       const x = (clientX - (left + width / 2)) / 2;
       const y = (clientY - (top + height / 2)) / 2;
       setPosition({ x, y });
